7233b06466c073ec334dd40aca4c5a38b350c06e
Brigita Bizjak <brigita.bizjak05@gmail.com>
2023-10-03 18:08:40 +0200

    Update with own api

--- src/requests.js
+++ src/requests.js
@@ -1,10 +1,11 @@
 const apiKey = process.env.REACT_APP_API_KEY;
+const serverUrl = process.env.REACT_APP_API_SERVER_URL;
 
 const requests = {
-  requestTopHeadlines: `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${apiKey}`,
-  requestTopHeadlinesBBC: `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${apiKey}`,
-  requestTopHeadlinesUS: `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`,
-  requestEverythingChatGPT: `https://newsapi.org/v2/everything?q=chatgpt&apiKey=${apiKey}`,
+  requestTopHeadlines: `${serverUrl}/topheadlines?sources=techcrunch&apiKey=${apiKey}`,
+  requestTopHeadlinesBBC: `${serverUrl}/topheadlines?sources=bbc-news&apiKey=${apiKey}`,
+  requestTopHeadlinesUS: `${serverUrl}/topheadlines?country=us&apiKey=${apiKey}`,
+  requestEverythingChatGPT: `${serverUrl}/everything?q=chatgpt&apiKey=${apiKey}`,
 };
 
 export default requests;
